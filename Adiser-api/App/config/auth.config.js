e163be46d1190f7ecaa9a4e7c3345d327fe95a35
Gadise Mustika <gadisee6@gmail.com>
2022-06-09 07:32:09 +0700

    make api post data from ocr result

--- Adiser-api/App/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: "adiser-secret-key"
-}
--- Adiser-api/App/config/db.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    HOST : "adiser:us-central1:adiser123",
-    USER :"root",
-    PASSWORD: "root123",
-    DB: "adiser",
-    dialect: "mysql"
-} ;
